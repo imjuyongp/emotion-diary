5a6b986f33476f44c35f959548a3fbd8c2a15472
imjuyongp <imjuyongp@gmail.com>
2026-02-06 17:22:54 +0900

    더미데이터 임시 삽입

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,45 @@
 import './App.css'
-import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import { useReducer } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Diary from './pages/Diary'
 import New from './pages/New'
 import Notfound from './pages/Notfound'
-import Button from './components/button'
-import Header from './components/Header'
+import Edit from './pages/Edit'
 
-import { getEmotionImage } from './utils/get-emotion-image'
+// 임시 일기 데이터
+const mockData = [
+  {
+    id: 1,
+    createdData : new Date().getTime(),
+    emotionId : 1,
+    content : "1번 일기 내용"
+  },
+  {
+    id: 2,
+    createdData : new Date().getTime(),
+    emotionId : 2,
+    content : "2번 일기 내용"
+  },
+];
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+function reducer(state, action) {
+  return state;
+}
+
+
 function App() {
 
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  }
+  const [data, dispatch] = useReducer(reducer, mockData);
 
   return (
     <>
-      <Header title={"Header"}
-        leftChild={<Button text={"Left"}/>}
-        rightChild={<Button text={"Right"}/>}/>
-
-      <Button 
-        text={123} 
-        onClick={() => {
-          console.log("123 버튼 클릭");
-        }}
-        type={"DEFAULT"} // 기본 버튼
-      />
-
-      <Button 
-        text={123} 
-        onClick={() => {
-          console.log("123 버튼 클릭");
-        }}
-        type={"POSITIVE"} // 긍정적인 버튼
-      />
-
-      <Button 
-        text={123} 
-        onClick={() => {
-          console.log("123 버튼 클릭");
-        }}
-        type={"NEGATIVE"} // 부정적인 버튼
-      />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
         <Route path="*" element={<Notfound />} />
+        <Route path="/edit/:id" element={<Edit />} />
       </Routes>
     </>
   )
