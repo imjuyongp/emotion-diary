1b463acdd6031fefa15b95f850c4663af3d0b10c
imjuyongp <imjuyongp@gmail.com>
2026-02-10 18:01:47 +0900

    new 페이지 ui 구현

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,6 +1,11 @@
+import Header from "../components/Header"
+import Button from "../components/Button"
+import Editor from "../components/Editor"
+
 const New = () => {
   return <div>
-    New
+    <Header title={"새 일기 쓰기"} leftChild={<Button text={"< 뒤로 가기"} />}/> 
+    <Editor />
   </div>
 }
 
